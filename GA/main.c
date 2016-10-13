3a46c49a79b5ede0bf1a5becf43151b1a972c30f
He11o_Liu <he11o_liu@macbook.lan>
2016-10-13 23:50:11 +0800

    Initial Commit

---
+++ GA/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  GA
+//
+//  Created by He11o_Liu on 2016/10/13.
+//  Copyright © 2016年 He11o_Liu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
